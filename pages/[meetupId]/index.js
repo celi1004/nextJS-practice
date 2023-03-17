beec9bd00f930ea99fb01afa1f64e0d848ebf558
unknown <2922268@device.hmc.co.kr>
2023-03-17 14:54:42 +0900

    add db and meta-data

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,27 +1,39 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import { MongoClient, ObjectId } from "mongodb";
+
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
     return (
-        <MeetupDetail 
-            image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
-            title="A First Meetup"
-            address="Some Street 5, 12345 City"
-            description="The meetup description"
-        />
+        <Fragment>
+            <Head>
+                <title>{props.meetupData.title}</title>
+                <meta name="description" content={props.meetupData.description} />
+            </Head>
+            <MeetupDetail 
+                image={props.meetupData.image}
+                title={props.meetupData.title}
+                address={props.meetupData.address}
+                description={props.meetupData.description}
+            />
+        </Fragment>
     );
 } 
 
 export async function getStaticPaths() {
+    const client = await MongoClient.connect("mongodb+srv://pjpp8588:dN7GcEjYvpxGQd8x@cluster0.8gd6qwr.mongodb.net/meetups?retryWrites=true&w=majority");
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const meetups = await meetupsCollection.find({}, {_id: 1}).toArray();
+
+    client.close();
+
     return {
         fallback: false, // 아래 해당사항 없는 params 입력 시 404에러 발생시킴
-        paths: [
-            { params: {
-                meetupId: "m1",
-            }},
-            { params: {
-                meetupId: "m2",
-            }}
-        ]
+        paths: meetups.map(meetup => ({ params: {meetupId: meetup._id.toString() }}))
     }
 } 
 
@@ -30,15 +42,22 @@ export async function getStaticProps(context) {
 
     const meetupId = context.params.meetupId;
 
+    const client = await MongoClient.connect("mongodb+srv://pjpp8588:dN7GcEjYvpxGQd8x@cluster0.8gd6qwr.mongodb.net/meetups?retryWrites=true&w=majority");
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const selectedMeetup = await meetupsCollection.findOne({_id: new ObjectId(meetupId) });
+
     return {
         props: {
             meetupData: {
-                image:"https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-                id: meetupId,
-                title: "A First Meetup",
-                address: "Some Street 5, 12345 City",
-                description: "The meetup description"
-            }
+                id: selectedMeetup._id.toString(),
+                title: selectedMeetup.title,
+                address: selectedMeetup.address,
+                image: selectedMeetup.image,
+                description: selectedMeetup.description
+            },
         }
     }
 }
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,4 +1,5 @@
-
+import Head from "next/head";
+import { Fragment } from "react";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm"
 
 function NewMeetupPage() {
@@ -17,7 +18,15 @@ function NewMeetupPage() {
         console.log(data);
     }
 
-    return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    return (
+        <Fragment>
+            <Head>
+                <title>Add a New Meetup</title>
+                <meta name="description" content="Add your own meetups and create amazing networking opportunities." />
+            </Head>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
+        </Fragment>
+    );
 }
 
 export default NewMeetupPage;
